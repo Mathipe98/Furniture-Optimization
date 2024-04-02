6ea99a35b81216f52b672c475df25a219f08cc18
Mathias Pettersen <mathipe@stud.ntnu.no>
2024-04-02 21:52:23 +0200

    Started adding type hints to cost func file

--- layoutCostFunctions.py
+++ layoutCostFunctions.py
@@ -4,24 +4,26 @@
 # -----------------------------------------------------------------------------------------
 import numpy as np
 import numpy.linalg as npla
+import shapely.affinity as saf  # type: ignore
+import shapely.geometry.linestring as sgls  # type: ignore
+import shapely.geometry.point as spt  # type: ignore
+
+# import scipy as sp
+import shapely.geometry.polygon as sgp  # type: ignore
+
 # ==> should install pyvisgraph
 import pyvisgraph as pvg
-# import scipy as sp
-import shapely.geometry.polygon as sgp
-import shapely.geometry.linestring as sgls
-import shapely.geometry.point as spt
-import shapely.affinity as saf
 
 
 def findPathPoly(sourceP, targetP, objList, layoutPoly):
     """
     calculating shortest path from sourceP point to targetP that avoid polygon shape obstacles
     sourceP/targetP - 2D points
-    objList - List of obstacle objects (polygons, each is a list of 2D points). 
+    objList - List of obstacle objects (polygons, each is a list of 2D points).
                 Should Contains the object's polygon and forward facing edge ???
     layoutPoly - Nx2 list of ordered vertices defining a 2D polygon of N vertices - room polygon layout
                 last point NEQ first point
-    =>>>>>>> Assuming polygons DO NOT intersect  
+    =>>>>>>> Assuming polygons DO NOT intersect
     """
 
     nObj = len(objList)
@@ -54,12 +56,16 @@ def findPathPoly(sourceP, targetP, objList, layoutPoly):
         pdistance = 0
         prev_point = spath[0]
         for point in spath[1:]:
-            pdistance += np.sqrt((prev_point.y - point.y) ** 2 + (prev_point.x - point.x) ** 2)
+            pdistance += np.sqrt(
+                (prev_point.y - point.y) ** 2 + (prev_point.x - point.x) ** 2
+            )
             prev_point = point
 
         shortest_path.append(spath)
         path_distance.append(pdistance)
-        dDist = np.sqrt((targetP[n][0] - sourceP[n][0]) ** 2 + (targetP[n][1] - sourceP[n][1]) ** 2)
+        dDist = np.sqrt(
+            (targetP[n][0] - sourceP[n][0]) ** 2 + (targetP[n][1] - sourceP[n][1]) ** 2
+        )
         direct_distance.append(dDist)
     # print('Shortest path distance: {}'.format(path_distance))
 
@@ -68,12 +74,13 @@ def findPathPoly(sourceP, targetP, objList, layoutPoly):
 
 # ------------------------------------------------------------------------------------------
 
-def calcLayoutClearance(objList, layoutPoly, entList):
+
+def calcLayoutClearance(objList: list[list[tuple[float, float]]], layoutPoly, entList):
     """
-    calculating layout polygons mean overlap 
+    calculating layout polygons mean overlap
     objList - List of obstacle objects (polygons)
                 Each object is assumed to represent the EXTENDED-bounding-box, i.e., including the extra gap
-                required around the object 
+                required around the object
     layoutPoly - Nx2 list of ordered vertices defining a 2D polygon of N vertices - room polygon layout
                last point NEQ first point
     entList - List of entrance line segments (2D points). Entrances should not be occluded
@@ -89,7 +96,7 @@ def calcLayoutClearance(objList, layoutPoly, entList):
     for n in range(nObj):
         objListSp.append(sgp.Polygon(objList[n]))
 
-    ovlpSum = 0
+    ovlpSum: float = 0
     for m in range(nObj - 1):
         for n in range(nObj):
             if m == n:
@@ -108,13 +115,16 @@ def calcLayoutClearance(objList, layoutPoly, entList):
 
 # ------------------------------------------------------------------------------------------
 
-def calcLayoutCirculation(objList, srcList, tgtList):
+
+def calcLayoutCirculation(
+    objList: list[list[tuple[float, float]]], srcList: list[list], tgtList: list[list]
+):
     """
-    calculating layout polygons accessibility from entrance (could be more than one entrance)  
+    calculating layout polygons accessibility from entrance (could be more than one entrance)
     objList - List of obstacle objects (polygons)
                 Each object is assumed to represent the EXTENDED-bounding-box, i.e., including the extra gap
-                required around the object 
-    src/tgt-List - pairs of points between which shortest path is calculated and compared to straight path 
+                required around the object
+    src/tgt-List - pairs of points between which shortest path is calculated and compared to straight path
     """
 
     nPairs = len(srcList)
@@ -123,19 +133,20 @@ def calcLayoutCirculation(objList, srcList, tgtList):
     sPath, lenPath, dirPath = findPathPoly(srcList, tgtList, objList, [])
 
     for n in range(nPairs):
-        pathRatioSum += (1 - dirPath[n] / lenPath[n])
+        pathRatioSum += 1 - dirPath[n] / lenPath[n]
 
     return pathRatioSum
 
 
 # ------------------------------------------------------------------------------------------
 
+
 def calcGroupRelation(objPos, membershipVec, dR):
     """
-    calculating object inter-group-relations: spread of objects from a group relative to space diagonal  
+    calculating object inter-group-relations: spread of objects from a group relative to space diagonal
     objPos - vector of objects' center (numpy array)
     membershipVec - vector of objects' membership association (integers)
-    dR - space diagonal (scalar) 
+    dR - space diagonal (scalar)
     """
 
     gSum = 0
@@ -143,23 +154,28 @@ def calcGroupRelation(objPos, membershipVec, dR):
 
     for i in range(nObj - 1):
         for j in range(i + 1, nObj):
-            gSum += 1.0 * (not (membershipVec[i] - membershipVec[j])) * npla.norm(np.array(objPos[i]) - np.array(objPos[j]))
+            gSum += (
+                1.0
+                * (not (membershipVec[i] - membershipVec[j]))
+                * npla.norm(np.array(objPos[i]) - np.array(objPos[j]))
+            )
 
-    gSum /= ((nObj - 1) * nObj / 2 * dR)
+    gSum /= (nObj - 1) * nObj / 2 * dR
 
     return gSum
 
 
 # ------------------------------------------------------------------------------------------
 
+
 #### Remaining ##
 def calcAlignment(backPos, walls, wallProbVec, dR):
     """
-    calculating object alignment, currently only w.r.t. supporting wall  
+    calculating object alignment, currently only w.r.t. supporting wall
     backPos - vector of objects' back position (numpy array)
-    walls - list of walls, each represented as a line (end points) 
+    walls - list of walls, each represented as a line (end points)
     wallProbVec - probability vector of objects' to stand against the wall
-    dR - space diagonal (scalar) 
+    dR - space diagonal (scalar)
     """
 
     #
@@ -180,15 +196,16 @@ def calcAlignment(backPos, walls, wallProbVec, dR):
             dP = np.append(dP, wLines[iW].distance(spt.Point(backPos[iO])))
         wSum += wallProbVec[iO] * min(dP)
 
-    wSum /= (nO * dR)
+    wSum /= nO * dR
     return wSum
 
 
 # ------------------------------------------------------------------------------------------
 
+
 def calcObjDistrib(objPos):
     """
-    calculating object distribution in space, also referred to as Rhythm 
+    calculating object distribution in space, also referred to as Rhythm
     """
 
     nObj = len(objPos)
@@ -214,6 +231,7 @@ def calcObjDistrib(objPos):
 
 # ------------------------------------------------------------------------------------------
 
+
 def calcViewFrust(objPairsPos, objPairsDir):
     """
     calculating viewing frustum of some pairs of objects, i.e., those objects must "see" each other
@@ -223,23 +241,24 @@ def calcViewFrust(objPairsPos, objPairsDir):
 
 # ------------------------------------------------------------------------------------------
 
+
 def calcFunctionality(impVec, objCatNum, catDesNum):
     """
-    calculating objects functionality importance and quantity 
+    calculating objects functionality importance and quantity
      impVec - vector of importance values
      objCatNum - amount of objects from each category in the layout (dict)
-     catDesNum - desired amount of objects from each category (dict)  
+     catDesNum - desired amount of objects from each category (dict)
     """
 
     nO = len(impVec)
 
-    fSum1 = np.sum(1. - impVec)
+    fSum1 = np.sum(1.0 - impVec)
     fSum1 /= nO
 
     fSum2 = 0
     for oc in objCatNum.keys():
         fSum2 += abs(objCatNum[oc] - catDesNum[oc])
-    fSum2 /= (1.0 * len(objCatNum))
+    fSum2 /= 1.0 * len(objCatNum)
 
     fSum = 0.5 * (fSum1 + fSum2)
     return fSum
@@ -247,10 +266,11 @@ def calcFunctionality(impVec, objCatNum, catDesNum):
 
 # ------------------------------------------------------------------------------------------
 
+
 def calcProportion(objVol, roomVol, desRatio):
     """
     calculating layout-volume-to-room-ratio
-     objVol: array of objects' area 
+     objVol: array of objects' area
      roomVol: room area
     """
 
@@ -264,11 +284,12 @@ def calcProportion(objVol, roomVol, desRatio):
 
 # ------------------------------------------------------------------------------------------
 
+
 def calcGoldenSec(objPos, roomRect, dR):
     """
     calculating objects location w.r.t. golden section lines
      objPos: objects' center position
-     roomRect: 4 points of room (or sub-area) rectangle  
+     roomRect: 4 points of room (or sub-area) rectangle
      dR: room diagonal
     """
 
@@ -313,6 +334,6 @@ def calcGoldenSec(objPos, roomRect, dR):
         dObjGs.append(min(dd))
 
     gP = np.sum(dObjGs)
-    gP /= (1.0 * dR * len(objPos))
+    gP /= 1.0 * dR * len(objPos)
 
     return gP
